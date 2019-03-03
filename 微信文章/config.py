8ce17358a9cfa09da171b424d0fd087850fa95e5
mzy <18565088712@163.com>
2019-03-03 23:31:47 +0800

    利用搜狗搜索爬取微信文章

---
+++ 微信文章/config.py
@@ -0,0 +1,12 @@
+# !/user/bin/env/python 
+# encoding:utf-8
+# Author : mzyang
+# @Date  : 2019/3/3
+
+
+MONGO_BD = 'wechat'
+MONGO_URL = 'localhost'
+KEYWORD = 'DOTA2'
+# 代理池地址 https://github.com/Germey/ProxyPool.git
+PROXY_POOL_URL = 'http://127.0.0.1:5000/get'
+MAX_COUNT = 5
